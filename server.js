d78242304c3571c44ae3313a0b133b958b5bbc90
Jafary Mdegela <jaffarymdegela@gmail.com>
2025-12-17 17:03:30 +0300

    working-git-ignore-env

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 import express from "express";
+import dotenv from "dotenv";
 const app = express();
-const port = 4000;
 
-app.listen(port, () => {
+dotenv.config();
+
+const PORT = process.env.PORT || 4001;
+
+app.get("/", (req, res) => {
+  res.send("Working autorotate");
+});
+
+app.listen(PORT, () => {
   console.log("Server is up and running");
 });
